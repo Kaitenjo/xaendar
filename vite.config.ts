1619c15ed2d98727284ce9a28776c1e781e3fc7b
Dario <dariospinosa123@gmail.com>
2026-05-10 20:20:36 +0200

    Feature: Browser Test Environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 
+console.log(path.resolve(__dirname, 'packages/signals/src'));
+
 export default defineConfig({
-  build: {
-    target: 'esnext',
-    lib: {
-      entry: path.resolve(__dirname, 'src/test.ts'),
-      fileName: () => `test.es.js`,
-      formats: ['es']
-    },
-    minify: false,
-    sourcemap: true,
-    outDir: path.resolve(__dirname, 'dist'),
+  root: 'src',
+  server: {
+    open: true,
+    port: 4200
+  },
+  resolve: {
+    alias: {
+      '@xaendar/common': path.resolve(__dirname, 'packages/common/src/public-api.ts'),
+      '@xaendar/signals': path.resolve(__dirname, 'packages/signals/src/public-api.ts'),
+    }
   }
-})
+});
